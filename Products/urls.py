5ff79df41390007c8cb80fe9f1195423ddd3b404
Your Name <you@example.com>
2026-03-09 01:45:05 +0600

    Fix URL name mismatch: rename second_page/id to second_page

--- Products/urls.py
+++ Products/urls.py
@@ -5,7 +5,7 @@ from .views import Products, next_page, second_page
 urlpatterns = [
     path('', Products, name='product'),
     path('next_page/', next_page, name='next_page'),
-    path('second_page/<int:id>/', second_page, name='second_page/id'),
+    path('second_page/<int:id>/', second_page, name='second_page'),
 
 
 ]
