a42275460bac3491813badcb78a51bc9c1b613e9
Your Name <you@example.com>
2026-03-08 23:57:09 +0600

    Fix tests: add setup for Car model

--- Products/tests.py
+++ Products/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Car  # Замените на правильное название вашей модели
 
-# Create your tests here.
+class CarPagesTest(TestCase):
+    def setUp(self):
+        # Создаем временную машину специально для теста
+        self.car = Car.objects.create(
+            name="Test Car",
+            year=2024,
+            color="Blue"
+        )
+
+    def test_next_page_status_code(self):
+        response = self.client.get(reverse('next_page'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_second_page_status_code(self):
+        # Теперь мы проверяем переход на страницу конкретной машины
+        response = self.client.get(reverse('second_page', args=[self.car.id]))
+        self.assertEqual(response.status_code, 200)
